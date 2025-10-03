74d88e4ef5c6bd214ab281bded6b9855a58ea702
PEDRO JUAN FRANCO GILO <pjuan@iel.unicamp.br>
2025-10-03 17:01:06 -0300

    Please enter the commit message for your changes. Lines starting

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: "export", // cria site estático para Netlify
+  images: {
+    unoptimized: true, // evita problemas com next/image no export
+  },
 };
 
 export default nextConfig;
